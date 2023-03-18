4031bf92b7cf9383eeb8213ee5802d177d87e941
Matias <matiasganduya@gmail.com>
2023-03-18 00:23:01 -0300

    Convertir euros a dolar

--- Proyecto1/Program.cs
+++ Proyecto1/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Proyecto1
@@ -20,18 +21,44 @@ namespace Proyecto1
             Circulo miCirculo2= new Circulo();//Declarar e iniciar en la misma linea
 
             Console.WriteLine(miCirculo2.calculoArea(9));
+
+            ConversorEuroDolar obj= new ConversorEuroDolar();
+
+            obj.cambiarValorEuro (1.05);
+            Console.WriteLine (obj.Convierte(50)+" Euros");
+
+            
         }
     }
 
 
     class Circulo
     {
-        double pi = 3.1216;
+        const double pi = 3.1216;
 
         public double calculoArea(int radio)
         {
             
             return pi * radio * radio;
+        }
+        
+    }
+
+    class ConversorEuroDolar
+    {
+
+        private double euro = 1.253;
+
+        public double Convierte(double cantidad)
+        {
+            return cantidad * euro;
+        }
+
+        public void cambiarValorEuro(double nuevoValor)
+        {
+            if (nuevoValor < 0) euro = 1.253;
+            else 
+            euro=nuevoValor;
         }
 
     }
